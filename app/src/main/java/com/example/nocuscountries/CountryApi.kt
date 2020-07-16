d5569fbf9eec31ffd83205d5f5a1a11a611fae3c
unknown <nelsonsaakekofi@gmail.com>
2020-07-16 16:29:13 +0000

    commit fixes to country api service retrofit annotation

--- app/src/main/java/com/example/nocuscountries/CountryApi.kt
+++ app/src/main/java/com/example/nocuscountries/CountryApi.kt
@@ -4,10 +4,11 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 interface CountryApiService{
 
-    @Get("all")
+    @GET("all")
     fun getCountriesInfo() : Call<ArrayList<CountryInfo>>
 
     companion object {
