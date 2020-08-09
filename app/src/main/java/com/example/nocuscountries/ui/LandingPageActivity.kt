3cd185216e7705837509cd1a7d9291c3a7cdbd9a
unknown <nelsonsaakekofi@gmail.com>
2020-08-09 23:40:51 +0000

    commit landing page

--- app/src/main/java/com/example/nocuscountries/ui/LandingPageActivity.kt
+++ app/src/main/java/com/example/nocuscountries/ui/LandingPageActivity.kt
@@ -1,11 +1,9 @@
 package com.example.nocuscountries.ui
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nocuscountries.R
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class LandingPageActivity : AppCompatActivity() {
 
@@ -14,9 +12,5 @@ class LandingPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_landing_page)
         setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
