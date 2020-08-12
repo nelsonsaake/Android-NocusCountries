2fcde3f48a52c306c10c626cfa7c535055da4d1d
unknown <nelsonsaakekofi@gmail.com>
2020-08-12 18:54:54 +0000

    landing page commit

--- app/src/main/java/com/example/nocuscountries/ui/LandingPageActivity.kt
+++ app/src/main/java/com/example/nocuscountries/ui/LandingPageActivity.kt
@@ -1,16 +1,25 @@
 package com.example.nocuscountries.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nocuscountries.R
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import kotlinx.android.synthetic.main.content_scrolling.*
 
 class LandingPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_landing_page)
+
         setSupportActionBar(findViewById(R.id.toolbar))
+
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+
+        allButton.setOnClickListener {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
     }
 }
